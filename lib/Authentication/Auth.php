db8e3c8f98215f9d464f75526f25333ff44eda06
Giovana Cassias <giovanacassias@outlook.com>
2025-10-04 17:07:29 -0300

    criação de página para login

--- lib/Authentication/Auth.php
+++ lib/Authentication/Auth.php
@@ -8,7 +8,7 @@ class Auth
 {
     public static function login($user): void
     {
-        $_SESSION['user']['id'] = $user->id;
+        $_SESSION['user']['id'] = $user->id; //aqui concentra as informações da sessão através de um log in
     }
 
     public static function user(): ?User
@@ -16,11 +16,13 @@ class Auth
         if (isset($_SESSION['user']['id'])) {
             $id = $_SESSION['user']['id'];
             return User::findById($id);
+            //se existe user e id na sessão, retornamos o user
         }
 
         return null;
     }
 
+    //verifica se tem uma sessão ativa
     public static function check(): bool
     {
         return isset($_SESSION['user']['id']) && self::user() !== null;
