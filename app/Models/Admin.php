1a5e53563a28fe732d67d8bb02e4787a946d5776
Giovana Cassias <giovanacassias@outlook.com>
2025-10-05 17:25:44 -0300

    Admin model created and populate.php updated to register two users - one of them as admin

---
+++ app/Models/Admin.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Core\Database\ActiveRecord\BelongsTo;
+use Core\Database\ActiveRecord\HasMany;
+use Core\Database\ActiveRecord\Model;
+
+/**
+ * @property int $id
+ * @property int $user_id
+ *
+ *
+ * */
+
+class Admin extends Model {
+    protected static string $table = 'admins';
+    protected static array $columns = ['user_id'];
+
+    protected array $errors = [];
+
+    //Verifica se o admin está vinculado a um usuário via a coluna user_id
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function validates(): void {
+        if ($this->user_id == 0) {
+            $this->addError('user_id', 'O user_id não pode ser nulo.');
+            return;
+        }
+    }
+
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+
+}
--- database/Populate/UsersPopulate.php
+++ database/Populate/UsersPopulate.php
@@ -2,6 +2,7 @@
 
 namespace Database\Populate;
 
+use App\Models\Admin;
 use App\Models\User;
 
 class UsersPopulate{
@@ -16,9 +17,30 @@ class UsersPopulate{
         ]);
 
         if ($user1->save()) {
-            echo "User saved!\n";
+            echo "User succesfully saved!\n";
         } else {
             var_dump($user1->errors);
+        }
+
+        $admin1 = new Admin(['user_id' => $user1->id]);
+
+        if ($admin1->save()) {
+            echo "Admin succesfully saved!\n";
+        } else {
+            var_dump($admin1->errors);
+        }
+
+        $user2 = new User([
+            'name' => 'user_example2',
+            'email' => 'user@exemplo2.com',
+            'password' => '987654',
+            'password_confirmation' => '987654'
+        ]);
+
+        if ($user2->save()) {
+            echo "User succesfully saved!\n";
+        } else {
+            var_dump($user2->errors);
         }
     }
 }
