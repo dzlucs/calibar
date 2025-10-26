2e2729397ca4b721048a6e2ebb882350b341b0f1
Giovana Cassias <giovanacassias@outlook.com>
2025-10-26 15:18:50 -0300

    Bug with paginator and button overlapping fixed

---
+++ tests/Acceptance/Drinks/DrinkCest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Acceptance\Drinks;
+
+use App\Models\Admin;
+use App\Models\Drink;
+use App\Models\User;
+use Tests\Acceptance\BaseAcceptanceCest;
+use Tests\Support\AcceptanceTester;
+
+class DrinkCest extends BaseAcceptanceCest
+{
+    private User $user;
+    private Admin $admin;
+    private Drink $drink;
+
+    private function setUp(): void
+    {
+        $this->user = new User([
+            'name' => 'Admin test',
+            'email' => 'admin@test.com',
+            'password' => '123',
+            'password_confirmation' => '123'
+        ]);
+        
+        $this->user->save();
+
+        $this->admin = new Admin([
+            'user_id' => $this->user->id
+        ]);
+
+        $this->admin->save();
+    }
+
+    public function create_drink_successfully(AcceptanceTester $page): void
+    {
+        $this->setUp();
+
+        $page->login($this->user->email, $this->user->password);
+
+        $page->amOnPage('/admin/drinks');
+        $page->waitForElementVisible('a[href="/admin/drinks/new"]', 10);
+        $page->scrollTo('a[href="/admin/drinks/new"]');
+
+        
+        $page->waitForText('Adicionar drink');
+        $page->click('a[href="/admin/drinks/new"]');
+
+        $page->fillField('drink_name', 'Drink de teste');
+        $page->fillField('drink_price', '49.90');
+
+        $page->click('Adicionar');
+        $page->waitForText('Drink registrado com sucesso!');
+    }
+}
