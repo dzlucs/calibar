e9d0741adc430629956eef07dc0bd0af001c8711
Giovana Cassias <giovanacassias@outlook.com>
2025-10-26 20:38:00 -0300

    test_should_create_new_drink() has been written and tested

---
+++ tests/Unit/Models/Users/DrinkTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Unit\Models\Drinks;
+
+use App\Models\Drink;
+use App\Models\Admin;
+use App\Models\User;
+use Tests\TestCase;
+
+class DrinkTest extends TestCase
+{
+    private Drink $drink;
+    private User $user;
+    private Admin $admin;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = new User([
+            'name' => 'Admin Test',
+            'email' => 'admin@test.com',
+            'password' => 'password123',
+            'password_confirmation' => 'password123'
+        ]);
+        $this->user->save();
+
+        $this->admin = new Admin([
+            'user_id' => $this->user->id
+        ]);
+        $this->admin->save();
+
+        $this->drink = new Drink([
+            'name' => 'drink test 1',
+            'price' => '49.90',  
+            'admin_id' => $this->admin->id
+        ]);
+        $this->drink->save();
+    }
+
+    public function test_should_create_new_drink(): void
+    {
+        $drink2 = $this->admin->drinks()->new([
+            'name' => 'drink test 2',
+            'price' => '39.90',  
+        ]);
+        $this->assertTrue($drink2->save());
+        $this->assertCount(2, Drink::all());
+    }
+}
