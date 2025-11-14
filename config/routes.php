5a57a105bf0e1944186f482335b2c2087d9de5ea
Giovana Cassias <giovanacassias@outlook.com>
2025-11-13 22:12:57 -0300

    trying one more time to fix permission bug on CI

--- config/routes.php
+++ config/routes.php
@@ -27,7 +27,8 @@ Route::middleware('auth')->group(function () {
         //CREATE
         Route::get('/admin/drinks/new', [DrinkController::class, 'new'])->name('drinks.new');//ok
         Route::post('/admin/drinks', [DrinkController::class, 'create'])->name('drinks.create');//ok
-        Route::post('/admin/drinks/{drink_id}/images', [DrinkController::class, 'createDrinkImage'])->name('drinks.image.create'); // create de uma imagem
+        Route::post(
+            '/admin/drinks/{drink_id}/images', [DrinkController::class, 'createDrinkImage'])->name('drinks.image.create');
 
         //READ
         Route::get('/admin/drinks', [DrinkController::class, 'index'])->name('drinks.index'); //ok
@@ -40,7 +41,8 @@ Route::middleware('auth')->group(function () {
 
         //DELETE
         Route::delete('/admin/drinks/{drink_id}', [DrinkController::class, 'destroy'])->name('drinks.destroy');//ok
-        Route::delete('/admin/drinks/{drink_id}/images', [DrinkController::class, 'destroyDrinkImage'])->name('drinks.image.destroy');// delete de uma imagem
+        Route::delete(
+            '/admin/drinks/{drink_id}/images', [DrinkController::class, 'destroyDrinkImage'])->name('drinks.image.destroy');// delete de uma imagem
     });
 
     Route::middleware('customer')->group(function () {
